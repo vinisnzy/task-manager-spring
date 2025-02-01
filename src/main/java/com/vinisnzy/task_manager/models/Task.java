76855dfb8a1151efb50fffde7a06e3edfab75f3b
Vinícius de Paula <manoseilacara123@gmail.com>
2025-02-01 16:10:18 -0300

    feat: create Task entity

---
+++ src/main/java/com/vinisnzy/task_manager/models/Task.java
@@ -0,0 +1,28 @@
+package com.vinisnzy.task_manager.models;
+
+import com.vinisnzy.task_manager.enums.TaskStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tasks")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status;
+}
