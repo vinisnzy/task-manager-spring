94b9823ae649938da17b2b82a4c02637f8ae837c
Vinícius de Paula <manoseilacara123@gmail.com>
2025-02-02 11:45:20 -0300

    feat: add RestExceptionHandler

---
+++ src/main/java/com/vinisnzy/task_manager/exceptions/RestExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.vinisnzy.task_manager.exceptions;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+        String error = "Resource not found";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError standardError = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(standardError);
+    }
+
+    @ExceptionHandler(ResourceInvalidDataException.class)
+    public ResponseEntity<StandardError> resourceInvalidData(ResourceInvalidDataException e, HttpServletRequest request) {
+        String error = "Invalid data";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError standardError = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(standardError);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<StandardError> genericError(RuntimeException e, HttpServletRequest request) {
+        String error = "Internal server error";
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        StandardError standardError = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(standardError);
+    }
+}
