c2da6403516a76dbfb06bc14c98e85a08eb31b84
Vinícius de Paula <manoseilacara123@gmail.com>
2025-02-02 11:41:35 -0300

    fix: update data type in status parameter

--- src/main/java/com/vinisnzy/task_manager/repositories/TaskRepository.java
+++ src/main/java/com/vinisnzy/task_manager/repositories/TaskRepository.java
@@ -1,5 +1,6 @@
 package com.vinisnzy.task_manager.repositories;
 
+import com.vinisnzy.task_manager.enums.TaskStatus;
 import com.vinisnzy.task_manager.models.entities.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -7,6 +8,6 @@ import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
     List<Task> findByTitleContaining(String title);
-    List<Task> findByStatus(String status);
-    List<Task> findByTitleContainingAndStatus(String title, String status);
+    List<Task> findByStatus(TaskStatus status);
+    List<Task> findByTitleContainingAndStatus(String title, TaskStatus status);
 }
